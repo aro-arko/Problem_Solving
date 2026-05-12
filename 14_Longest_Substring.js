4e1bb9a464611aaae736ce9b795644147b9b3948
Abidur Rahman Arko <aroarko.sd@gmail.com>
2026-05-12 17:03:24 +0800

    add function to find the length of the longest substring with all unique characters

---
+++ 14_Longest_Substring.js
@@ -0,0 +1,21 @@
+function longestUniqueSubstring(str) {
+  let set = new Set();
+
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < str.length; right++) {
+    while (set.has(str[right])) {
+      set.delete(str[left]);
+      left++;
+    }
+
+    set.add(str[right]);
+
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+console.log(longestUniqueSubstring("abcabcbb"));
